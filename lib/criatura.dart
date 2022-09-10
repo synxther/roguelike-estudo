31c5e414524c6c451126e7bad7c41da5174c0b7d
synxther <synxther@protonmail.com>
2022-09-09 23:01:49 -0300

    melhorias no codigo e correção de bug contagem de passos

--- lib/criatura.dart
+++ lib/criatura.dart
@@ -6,12 +6,13 @@ import 'package:roguelike/ponto_2d.dart';
 // Classe Criatura é derivada (filha) da classe Personagem
 class Criatura extends Personagem {
   // Constantes
-  static final int FICAR_PARADO = 0;
-  static final int MOVER_BAIXO = 1;
-  static final int MOVER_CIMA = 2;
-  static final int MOVER_DIREITA = 3;
-  static final int MOVER_ESQUERDA = 4;
-  static final int QUANTIDADE_MOVIMENTOS = 5;
+  // static final int FICAR_PARADO = 0;
+  // static final int MOVER_BAIXO = 1;
+  // static final int MOVER_CIMA = 2;
+  // static final int MOVER_DIREITA = 3;
+  // static final int MOVER_ESQUERDA = 4;
+  /// isso é redundante uma vez que será usado apenas uma vez
+  // static final int QUANTIDADE_MOVIMENTOS = 5;
   static final String SIMBOLO_CRIATURA = "Z";
 
   // Variável privada para criar números aleatório
@@ -30,17 +31,35 @@ class Criatura extends Personagem {
     // criar um número aleatório entre 0 e 100,
     // logo em seguida, calcula o resto da divisão por 5,
     // ou seja, só podemos ter os seguintes valores: 0, 1, 2, 3 e 4
-    var direcao = _aleatorio.nextInt(1000) % QUANTIDADE_MOVIMENTOS;
+    var direcao = _aleatorio.nextInt(1000) % 5;
 
     // desloca a criatura conforme o valor aleatório gerado
-    if (direcao == FICAR_PARADO) {
+    //a alteração ocorreu pela causa do jogador
+    switch(direcao) {
+      case 0:
       mover(mundo, 0, 0);
-    } else if (direcao == MOVER_BAIXO) {
+      break;
+      case 1:
       mover(mundo, 0, 1);
-    } else if (direcao == MOVER_CIMA) {
+      break;
+      case 2: 
       mover(mundo, 0, -1);
-    } else if (direcao == MOVER_DIREITA) {
+      break;
+      case 3:
       mover(mundo, 1, 0);
-    } else if (direcao == MOVER_ESQUERDA) mover(mundo, -1, 0);
+      break;
+      case 4: 
+      mover(mundo, -1, 0);
+      break;
+    }
+    // if (direcao == FICAR_PARADO) {
+    //   mover(mundo, 0, 0);
+    // } else if (direcao == MOVER_BAIXO) {
+    //   mover(mundo, 0, 1);
+    // } else if (direcao == MOVER_CIMA) {
+    //   mover(mundo, 0, -1);
+    // } else if (direcao == MOVER_DIREITA) {
+    //   mover(mundo, 1, 0);
+    // } else if (direcao == MOVER_ESQUERDA) mover(mundo, -1, 0);
   }
 }
--- lib/jogador.dart
+++ lib/jogador.dart
@@ -6,10 +6,10 @@ import 'package:roguelike/ponto_2d.dart';
 // Classe Jogador é derivada (filha) da classe Personagem
 class Jogador extends Personagem {
   // Constantes
-  static final String TECLA_CIMA = "W";
-  static final String TECLA_BAIXO = "S";
-  static final String TECLA_DIREITA = "D";
-  static final String TECLA_ESQUERDA = "A";
+  // static final String TECLA_CIMA = "W";
+  // static final String TECLA_BAIXO = "S";
+  // static final String TECLA_DIREITA = "D";
+  // static final String TECLA_ESQUERDA = "A";
   static final String SIMBOLO_JOGADOR = "@";
 
   int _vidas; // Variável privada (_nomeDaVariavel)
@@ -42,14 +42,34 @@ class Jogador extends Personagem {
     // Aguarda entrada pelo console e converte para maiusculo
     var entrada = stdin.readLineSync().toUpperCase();
 
-    // Verifica se o jogador digitou WASD para move-lo
-    if (entrada == TECLA_DIREITA) {
+    // Verifica se o jogador digitou WASD para move-lo trocamos o if and else por switch case simplismente por ficar com o codigo mais limpo e pelo o fato de a verificação é de apenas uma variavel logo o switch acaba sendo mais apropriado para o caso 
+    switch(entrada) {
+      case "D":
       mover(mundo, 1, 0);
-    } else if (entrada == TECLA_ESQUERDA) {
+      break;
+      case "A":
       mover(mundo, -1, 0);
-    } else if (entrada == TECLA_CIMA) {
+      break;
+      case "W":
       mover(mundo, 0, -1);
-    } else if (entrada == TECLA_BAIXO) mover(mundo, 0, 1);
+      break;
+      case "S": 
+      mover(mundo, 0, 1);
+      break;
+
+      default:
+      print("valor invalido tente novamente");
+      // o jogo tem um bug que ao apertar enter sem andar ele conta como passos msm que eu n ande para isso adcionamos um default com a seguinte linha
+      passos--;
+    }
+    // Verifica se o jogador digitou WASD para move-lo
+    // if (entrada == TECLA_DIREITA) {
+    //   mover(mundo, 1, 0);
+    // } else if (entrada == TECLA_ESQUERDA) {
+    //   mover(mundo, -1, 0);
+    // } else if (entrada == TECLA_CIMA) {
+    //   mover(mundo, 0, -1);
+    // } else if (entrada == TECLA_BAIXO) mover(mundo, 0, 1);
 
     // incrementa o número de passos do jogador
     passos++;
